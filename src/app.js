ea9e70ad7f52e2c7f2e1ce7389d7e18bf1297a39
unknown <kpnba90@gmail.com>
2019-05-02 11:26:08 -0400

    add whitelist cors-option

--- src/app.js
+++ src/app.js
@@ -8,12 +8,21 @@ const { NODE_ENV } = require('./config')
 const app = express()
 
 // const morganOption = (process.env.NODE_ENV === 'production')
-const morganOption = (NODE_ENV === 'production')  
-  ? 'tiny'
-  : 'common';
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
+const whitelist = ['http://localhost:3000', 'http://my-project.com'];
+const options = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+  
+app.use(cors(options));
 app.use(morgan(morganOption))
-app.use(cors())
 app.use(helmet())
 
 app.get('/', (req, res) => {

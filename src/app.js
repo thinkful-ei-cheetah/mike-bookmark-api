6d91c55386ea0a5186b7b7f3db8c3cbccaf4a25a
unknown <kpnba90@gmail.com>
2019-05-05 17:35:32 -0400

    bookmark

--- src/app.js
+++ src/app.js
@@ -4,41 +4,24 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const validateBearerToken = require('./validate-bearer-token')
+const errorHandler = require('./error-handler')
+const bookmarksRouter = require('./bookmarks/bookmarks-router')
 
 const app = express()
 
-// const morganOption = (process.env.NODE_ENV === 'production')
-const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test'
+}))
 
-const whitelist = ['http://localhost:3000', 'http://my-project.com'];
-const options = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-  
-app.use(cors(options));
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(cors());
+app.use(helmet());
+app.use(validateBearerToken);
+app.use(bookmarksRouter);
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
-})
-
-app.use(function errorHandler(error, req, res, next) {
-  let response
-  // if (process.env.NODE_ENV === 'production') {
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
-  } else {
-    console.error(error)
-    response = { message: error.message, error }
-  }
-  res.status(500).json(response)
-})
+});
 
 module.exports = app
